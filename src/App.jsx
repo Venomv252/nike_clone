bc719c7351cd2de630a7f429c2c9b26b302266db
Venomv252 <aryanraj9931614200@gmail.com>
2025-05-03 22:00:50 +0530

    day 1 final chnage

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import "./App.css";
 import { Nav1, Nav2} from "./Nav.jsx";
 import { Footer } from "./Footer.jsx";
+import { First } from "./first.jsx";
 
 function App() {
   return (
     <>
       <Nav1 />
       <Nav2 />
+      <First />
       <Footer />
     </>
   );
---
+++ src/first.jsx
@@ -0,0 +1,16 @@
+import "./first.css";
+export function First() {
+  return (
+    <div className="first_main">
+      <img
+        className="first_main_img"
+        src="https://static.nike.com/a/images/f_auto/dpr_1.3,cs_srgb/w_1424,c_limit/b5497bf1-1781-4ae7-87bc-5f927281bf2a/nike-just-do-it.png"
+      />
+      Nike 24.7
+      <h1>Collection TAILORED FOR ALL-DAY COMFORT</h1>
+      All-new colours, polished looks, luxurious feels.
+      <button>Shop</button>
+    </div>
+  );
+}
+//   https://static.nike.com/a/images/f_auto/dpr_1.3,cs_srgb/h_965,c_limit/66211d16-1eb7-4921-9c74-fec4315134ea/nike-just-do-it.jpg
